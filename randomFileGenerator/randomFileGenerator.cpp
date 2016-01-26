79ab8ba83f1422a38a1b832b4379c6681dba196e
Nantas Nardelli <nantas.nardelli@gmail.com>
2016-01-26 23:18:18 +0000

    Add output filename argument

--- randomFileGenerator/randomFileGenerator.cpp
+++ randomFileGenerator/randomFileGenerator.cpp
@@ -5,15 +5,17 @@
 #include <stdint.h>
 #include <random>
 #include <chrono>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char *argv[]){
-  if (argc != 2){
-    cout<<"usage: " << argv[0] << " <file size (bytes)>\n";
+  if (argc != 3){
+    cout<<"usage: " << argv[0] << " <file size (bytes)> <output file>\n";
     return -1;
   }
   int filesize = atoi(argv[1]);
+  std::string filename = argv[2];
 
   unsigned seed1 = std::chrono::system_clock::now().time_since_epoch().count();
 
@@ -21,7 +23,7 @@ int main(int argc, char *argv[]){
 	std::uniform_int_distribution<int> distribution(0,255);
 
 	fstream fs;
-	fs.open("randomFile.bin", fstream::out | fstream::trunc);
+	fs.open(filename.c_str(), fstream::out | fstream::trunc);
 
 	for (int i=0; i<filesize; i++)
 		fs << (uint8_t) distribution(generator);;
