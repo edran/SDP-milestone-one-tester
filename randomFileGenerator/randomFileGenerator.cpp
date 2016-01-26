8a9760caa5956f40a2defc75a17e1b791def245b
Nantas Nardelli <nantas.nardelli@gmail.com>
2016-01-26 23:08:27 +0000

    Make the randomFile generator actually random

--- randomFileGenerator/randomFileGenerator.cpp
+++ randomFileGenerator/randomFileGenerator.cpp
@@ -4,19 +4,21 @@
 #include <iostream>
 #include <stdint.h>
 #include <random>
+#include <chrono>
 
 using namespace std;
 
 int main(int argc, char *argv[]){
-	if (argc != 2){
-		cout<<"usage: " << argv[0] << " <file size (bytes)>\n";
-		return -1;
-	}
-	int filesize = atoi(argv[1]);
+  if (argc != 2){
+    cout<<"usage: " << argv[0] << " <file size (bytes)>\n";
+    return -1;
+  }
+  int filesize = atoi(argv[1]);
 
-	std::default_random_engine generator;
+  unsigned seed1 = std::chrono::system_clock::now().time_since_epoch().count();
+
+  std::default_random_engine generator(seed1);
 	std::uniform_int_distribution<int> distribution(0,255);
-
 
 	fstream fs;
 	fs.open("randomFile.bin", fstream::out | fstream::trunc);
